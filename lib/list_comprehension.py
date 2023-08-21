1d1d8e035d81087fa78aef765e7aed3fb80cbff5
B-atuti <brian.atuti@student.moringaschool.com>
2023-08-21 13:10:40 +0300

    complete task

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -6,10 +6,10 @@ def return_evens(num_list):
 def make_exclamation(sentence_list):
     return [n + '!' for n in sentence_list]
 
-numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1,3,5,7,8,9]
 even_numbers = return_evens(numbers)
 print("Even numbers:", even_numbers)
 
-sentences = ["Hello there", "Python is wow"]
+sentences = ["Hello", "I'm doing great", "Python is fun"]
 exclamations = make_exclamation(sentences)
 print("Exclamations:", exclamations)
